11f885de990a19ca4a8c92d2628a0d37db5ce30f
saulius <sjsaulius@gmail.com>
2025-05-21 15:18:20 +0300

    initial project

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import CSSBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <CssBaseline />
+      <h1>TO-DO</h1>
+    </>
+  );
+}
+
+export default App;
